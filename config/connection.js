d0a69ab53727ec383a8974aff578262f11138c71
David Rohnow <drohnow@Davids-MacBook-Pro.local>
2017-12-06 15:59:09 -0800

    working app; needs css and html structure

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// Set up MySQL connection to the burgers_db
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "Trek1515",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+
+});
+
+
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('hawaiin burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', false);
+
